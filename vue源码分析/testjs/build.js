8a5be536573b2f2ac4908bb2449440c549dbfc4e
1625619520@qq.com <1625619520@qq.com>
2020-03-26 00:08:01 +0800

    源码准备工作

---
+++ vue源码分析/testjs/build.js
@@ -0,0 +1,14 @@
+/*
+ * @Descripttion: 🐉
+ * @Author: xinxin
+ * @Date: 2020-03-25 22:28:39
+ * @LastEditTime: 2020-03-25 22:33:25
+ */
+console.log(process.argv);
+
+if (process.argv[2]) {
+    const filters = process.argv[2].split(',')
+    console.log(filters,process.argv[2])
+    
+  } else {
+  }
